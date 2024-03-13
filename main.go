a341c3a72311654412411e5cda1250663f20bb1c
Douglas Headley <dheadley@eliassen.com>
2024-03-13 11:23:08 -0700

    idempotent and time format changes

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/mehanizm/airtable"
@@ -25,36 +27,12 @@ func chunkRecords(records []*airtable.Record, chunkSize int) (chunks [][]*airtab
 
 func main() {
 	_ = godotenv.Load(".envrc")
-	// Create a new Slack Client
-	slackClient := slack.New(os.Getenv("SLACK_API_TOKEN"))
 
-	// Create a new Airtable Client
+	slackClient := slack.New(os.Getenv("SLACK_API_TOKEN"))
 	airtableClient := airtable.NewClient(os.Getenv("AIRTABLE_API_KEY"))
 
-	// table := airtableClient.Table(os.Getenv("AIRTABLE_TABLE_NAME"))
-
-	// schema, err := airtableClient.GetBaseSchema("your_database_ID").Do()
-	bases, err := airtableClient.GetBases().WithOffset("").Do()
-	for _, base := range bases.Bases {
-		fmt.Println(base)
-	}
-	// Pull all Slack channel messages
-	// historyParams := slack.NewHistoryParameters()
 	table := airtableClient.GetTable(os.Getenv("AIRTABLE_BASE_ID"), os.Getenv("AIRTABLE_TABLE_ID"))
-	// slackClient.GetConversationHistory(params *slack.GetConversationHistoryParameters)
 
-	type SlackMessage struct {
-		MessageID   string
-		Username    string
-		Text        string
-		ChannelID   string
-		ChannelName string
-		Timestamp   string
-		UserID      string
-		Reactions   string
-		Attachments string
-		ThreadTS    string
-	}
 	records, err := table.GetRecords().Do()
 	if err != nil {
 		panic(err)
@@ -74,18 +52,43 @@ func main() {
 		}
 		response, _ := slackClient.GetConversationHistory(lol)
 		// fmt.Println(response)
-
 		if channel.Name != "mcdaniel-sewer-and-drain" {
 			continue
 		}
 		records := airtable.Records{}
 		for _, slackMessage := range response.Messages {
+			msgID := slackMessage.Msg.ClientMsgID
+			existigRecords, err := table.GetRecords().WithFilterFormula(fmt.Sprintf("{Message ID} = '%s'", msgID)).Do()
+			if msgID == "" {
+				fmt.Println("Don't want a blank record")
+				continue
+			}
+			if len(existigRecords.Records) > 1 {
+				fmt.Println("already have that record")
+				continue
+			}
+			user, err := slackClient.GetUserInfo(slackMessage.User)
+			if err != nil {
+
+				fmt.Printf("Could not retrieve user: %s\n", err)
+			} else {
+				err = nil
+			}
+			floatTime, err := strconv.ParseFloat(slackMessage.Timestamp, 64)
+			if err != nil {
+				panic(err)
+			}
+			intTime := int64(floatTime)
+			tm := time.Unix(intTime, 0)
+			pst, err := time.LoadLocation("America/Los_Angeles")
+			tm.In(pst)
 			airTableRow := airtable.Record{
 				Fields: map[string]interface{}{
 					"Message ID": slackMessage.Msg.ClientMsgID,
-					"Username":   slackMessage.Username,
+					"Username":   user.Name,
 					"Message":    slackMessage.Text,
 					"Channel":    channel.Name,
+					"Timestamp":  tm.Format("January 2, 2006 - 03:04 PM"),
 				},
 			}
 			records.Records = append(records.Records, &airTableRow)
